df9062e11ff6babee923976735cd197f779bb0b1
Chris Mears <chris.mears@gmail.com>
2020-11-23 19:40:13 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Brewery review and beer style Natural Language Processing',
+    author='Chris J Mears',
+    license='MIT',
+)
